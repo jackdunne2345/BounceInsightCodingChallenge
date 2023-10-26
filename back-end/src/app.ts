f1caeb658238a13bc5dc38626bcf539e3fac19da
jack dunne <jackzoo123@outlook.com>
2023-10-26 19:49:48 +0100

    heroku deploy one

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,12 +2,22 @@ import express, { Request, Response } from 'express';
 import axios from 'axios';
 import cors from 'cors';
 import readline from 'readline';
+const path = require('path')
 const app = express();
-const port = 3000;
+const port = process.env.port||3000;
+
+
+
 
 app.use(cors());
 app.use(express.json());
 
+
+app.use(express.static(path.join(__dirname, '../../front-end/dist'))) 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../front-end/dist', 'index.html'));
+});
 
 app.post('/getCountryInfo', async (req: Request, res: Response) => {
   try {
